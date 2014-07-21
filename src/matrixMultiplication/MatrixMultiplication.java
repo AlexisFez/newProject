ecf599d4a7ad5112bdd57e0605410a5058e6e984
Alexis <alex.melliot@gmail.com>
2014-07-21 20:37:45 +0300

    Fixing logic

--- src/matrixMultiplication/MatrixMultiplication.java
+++ src/matrixMultiplication/MatrixMultiplication.java
@@ -1,48 +1,42 @@
 package matrixMultiplication;
 
-import com.sun.org.apache.xpath.internal.SourceTree;
-
 import java.util.Arrays;
 
 public class MatrixMultiplication {
 
-    private int counterAForX = 0;
     private int counterAForY = 0;
 
     private int counterBForX = 0;
-    private int counterBForY = 0;
 
     int counterForDestX = 0;
     int counterForDestY = 0;
 
-    public int[] matrixMultiplicator(int matrixA[][], int matrixB[][]) {
+    public void matrixMultiplicator(int matrixA[][], int matrixB[][]) {
 
         int matrixResult[][] = new int[matrixA.length][matrixB[0].length];
 
-        for (; counterForDestX < matrixA.length; counterForDestX++, counterAForX++) {
+        for (; counterForDestX < matrixA.length; counterForDestX++) {
 
-            for (; counterForDestY < matrixB[0].length; counterForDestY++, counterBForY++) {
+            for (; counterForDestY < matrixB[0].length; counterForDestY++) {
 
-                for (int i = 0; i < matrixB[0].length; i++) {
+                for (int index = 0; index < matrixB[0].length; index++) {
 
-                    matrixResult[counterForDestX][counterForDestY] += matrixA[counterForDestX][counterAForY + i] * matrixB[counterBForX + i][counterForDestY];
+                    matrixResult[counterForDestX][counterForDestY] += matrixA[counterForDestX][counterAForY + index] * matrixB[counterBForX + index][counterForDestY];
 
                 }
 
             }
 
-            counterBForY = 0;
-
             counterForDestY = 0;
 
         }
 
-        System.out.println("matrixResult[0] " + Arrays.toString(matrixResult[0]));
-        System.out.println("matrixResult[1] " + Arrays.toString(matrixResult[1]));
-        System.out.println("matrixResult[2] " + Arrays.toString(matrixResult[2]));
-        System.out.println("matrixResult[3] " + Arrays.toString(matrixResult[3]));
+        System.out.println("Result's array is: ");
 
-        return matrixResult[0];
+        for (int arrays = 0; arrays < matrixResult.length; arrays++){
+            System.out.println(Arrays.toString(matrixResult[arrays]));
+        }
+
     }
 
 
@@ -63,7 +57,7 @@ public class MatrixMultiplication {
 
         MatrixMultiplication matrixMultiplication = new MatrixMultiplication();
 
-        Arrays.toString(matrixMultiplication.matrixMultiplicator(arrayA, arrayB));
+        matrixMultiplication.matrixMultiplicator(arrayA, arrayB);
 
     }
 
