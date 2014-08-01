21d5e4946ef7b38d4adac64af8a2526eca7eeb38
Alexis <alex.melliot@gmail.com>
2014-08-01 11:10:37 +0300

    SoftReferenceExample

---
+++ src/main/java/Examples/SoftReferenceExample.java
@@ -0,0 +1,19 @@
+package Examples;
+
+import java.lang.ref.SoftReference;
+import java.util.ArrayList;
+import java.util.List;
+
+public class SoftReferenceExample {
+    public static void main(String[] args) throws InterruptedException {
+        List<SoftReference<byte[]>> list = new ArrayList<>();
+        while (true) {
+            System.out.println();
+            for (SoftReference<byte[]> ref : list) {
+                System.out.print(ref.get() == null ? "*" : "E");
+            }
+            list.add(new SoftReference<>(new byte[10_000_000]));
+            Thread.sleep(100);
+        }
+    }
+}
