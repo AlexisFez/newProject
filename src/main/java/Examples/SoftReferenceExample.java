33d7379b41053344d95d8c98427edb411cad51d7
Alexis <alex.melliot@gmail.com>
2014-08-01 11:15:39 +0300

    Soft reference

--- src/main/java/Examples/SoftReferenceExample.java
+++ src/main/java/Examples/SoftReferenceExample.java
@@ -5,11 +5,11 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class SoftReferenceExample {
-    public static void main(String[] args) throws InterruptedException {
+    public static void main(String[] args) throws InterruptedException{
         List<SoftReference<byte[]>> list = new ArrayList<>();
         while (true) {
             System.out.println();
-            for (SoftReference<byte[]> ref : list) {
+            for(SoftReference<byte[]> ref : list){
                 System.out.print(ref.get() == null ? "*" : "E");
             }
             list.add(new SoftReference<>(new byte[10_000_000]));
